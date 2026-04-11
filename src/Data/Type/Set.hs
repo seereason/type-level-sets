cbcfb6df205a9971420f400076d9d6e9fb4394b3
Clifford Beshers <beshers@seereason.com>
2026-04-11 12:42:27 -0700

    remove deprecated TypeInType

--- src/Data/Type/Set.hs
+++ src/Data/Type/Set.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GADTs, DataKinds, KindSignatures, TypeOperators, TypeFamilies,
              MultiParamTypeClasses, FlexibleInstances, PolyKinds,
              FlexibleContexts, UndecidableInstances, ConstraintKinds,
-             ScopedTypeVariables, TypeInType #-}
+             ScopedTypeVariables #-}
 {-# OPTIONS -Wno-unticked-promoted-constructors -Wno-unused-imports #-}
 
 module Data.Type.Set (Set(..), TSet, Union, Unionable, TUnion, Intersection, Difference, union, quicksort, append,
